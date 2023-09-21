d4fe3a0e5e3245361ddc1bf674b275ab160f3df7
jinwoong <baseball0020@gmail.com>
2023-09-21 20:48:40 +0900

    Initial Commit

---
+++ CatAPI/CatAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatAPIApp.swift
+//  CatAPI
+//
+//  Created by jinwoong Kim on 2023/09/21.
+//
+
+import SwiftUI
+
+@main
+struct CatAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
